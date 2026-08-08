37268e1770522f0aef96ea11d004b84b84aab5fd
Lucas Lagman <lucaslagman28@outlook.com>
2026-08-08 19:35:50 +0800

    Create workflow/CreateWorkspace.java
    Renamed the workflows to workflow.

---
+++ src/main/java/exxo/app/workflow/CreateWorkspace.java
@@ -0,0 +1,17 @@
+package exxo.app.workflow;
+
+import java.util.Scanner;
+
+public class CreateWorkspace {
+
+    private final Scanner reader;
+
+    private CreateWorkspace() {
+        this.reader = new Scanner(System.in);
+    }
+
+    public static CreateWorkspace start() {
+        return new CreateWorkspace();
+    }
+
+}
