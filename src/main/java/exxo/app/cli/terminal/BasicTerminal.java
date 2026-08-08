040e0989ead5817bf186b449366c2c96edd68a70
Lucas Lagman <lucaslagman28@outlook.com>
2026-08-08 21:27:13 +0800

    Update BasicTerminal.java
    Added a protected convenience constructor that accepts a writer.

--- src/main/java/exxo/app/cli/terminal/BasicTerminal.java
+++ src/main/java/exxo/app/cli/terminal/BasicTerminal.java
@@ -2,8 +2,10 @@ package exxo.app.cli.terminal;
 
 import exxo.app.cli.Console;
 
-import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.io.Writer;
 
 import java.util.Objects;
@@ -20,7 +22,17 @@ public class BasicTerminal implements Console {
     }
     
     protected BasicTerminal(Reader reader) {
-        this(reader, new PrintWriter(System.out));
+        this(
+            reader, 
+            new PrintWriter(System.out)
+        );
+    }
+    
+    protected BasicTerminal(Writer writer) {
+        this(
+            new BufferedReader(new InputStreamReader(System.in)), 
+            writer
+        );
     }
     
     public static BasicTerminal of(Reader reader, Writer writer) {
