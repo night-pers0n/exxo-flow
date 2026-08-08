6e98fc140a3b96a4b3edc07fd92b52e9917eecae
Lucas Lagman <lucaslagman28@outlook.com>
2026-08-08 20:20:39 +0800

    Update BasicTerminal.java
    Added the of() factory method.

--- src/main/java/exxo/app/cli/terminal/BasicTerminal.java
+++ src/main/java/exxo/app/cli/terminal/BasicTerminal.java
@@ -3,6 +3,9 @@ package exxo.app.cli.terminal;
 import exxo.app.cli.Console;
 
 import java.io.Writer;
+import java.io.PrintWriter;
+
+import java.util.Objects;
 
 public class BasicTerminal implements Console {
     
@@ -10,6 +13,10 @@ public class BasicTerminal implements Console {
     
     protected BasicTerminal(Writer writer) {
         this.writer = writer;
+    }
+    
+    public static BasicTerminal of(Writer writer) {
+        return new BasicTerminal(Objects.requireNonNull(writer));
     }
     
     @Override
