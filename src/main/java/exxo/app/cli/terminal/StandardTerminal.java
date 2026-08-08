3457916c40e1db5b3f5f68010114c4ab4505870e
Lucas Lagman <lucaslagman28@outlook.com>
2026-08-08 22:09:10 +0800

    Update StandardTerminal.java
    Added the of(reader, writer) factory method.

--- src/main/java/exxo/app/cli/terminal/StandardTerminal.java
+++ src/main/java/exxo/app/cli/terminal/StandardTerminal.java
@@ -3,10 +3,19 @@ package exxo.app.cli.terminal;
 import java.io.Reader;
 import java.io.Writer;
 
+import java.util.Objects;
+
 public class StandardTerminal extends Terminal {
     
     private StandardTerminal(Reader reader, Writer writer) {
         super(reader, writer);
+    }
+    
+    public static StandardTerminal of(Reader reader, Writer writer) {
+        return new StandardTerminal(
+            Objects.requireNonNull(reader), 
+            Objects.requireNonNull(writer)
+        );
     }
     
 }
