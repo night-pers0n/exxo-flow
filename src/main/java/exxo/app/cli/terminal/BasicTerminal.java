28a018f8e1a09e653f2a5113e626ce20f49b53cb
Lucas Lagman <lucaslagman28@outlook.com>
2026-08-08 20:14:54 +0800

    Update BasicTerminal.java
    Added a protected constructor that accepts a writer.

--- src/main/java/exxo/app/cli/terminal/BasicTerminal.java
+++ src/main/java/exxo/app/cli/terminal/BasicTerminal.java
@@ -2,7 +2,15 @@ package exxo.app.cli.terminal;
 
 import exxo.app.cli.Console;
 
+import java.io.Writer;
+
 public class BasicTerminal implements Console {
+    
+    private final Writer writer;
+    
+    protected BasicTerminal(Writer writer) {
+        this.writer = writer;
+    }
     
     @Override
     public void print(String text) {
