85ca911789668eb0f9d9764b00bd017bc84ea0ac
Lucas Lagman <lucaslagman28@outlook.com>
2026-08-08 19:59:35 +0800

    Update BasicTerminal.java
    Added an empty implementation of the Console.print(text) method.

--- src/main/java/exxo/app/cli/terminal/BasicTerminal.java
+++ src/main/java/exxo/app/cli/terminal/BasicTerminal.java
@@ -3,4 +3,9 @@ package exxo.app.cli.terminal;
 import exxo.app.cli.Console;
 
 public class BasicTerminal implements Console {
+    
+    @Override
+    public void print(String text) {
+    }
+    
 }
